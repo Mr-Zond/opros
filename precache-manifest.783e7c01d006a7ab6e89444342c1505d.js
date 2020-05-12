ca10ca47ee6dda5b5970047a99ead7d3cf218658
Mr-Zond <gorgiser@gmail.com>
2020-05-12 18:54:34 +0300

    Updates

---
+++ precache-manifest.783e7c01d006a7ab6e89444342c1505d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f220cf1060f5a7f7fc4edc7a32c627e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2a2663a181ee3bdf622",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "e2a2663a181ee3bdf622",
+    "url": "./static/js/2.05f1f93b.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.05f1f93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b1a8a957cd448446fc3",
+    "url": "./static/js/main.b7c3cb8f.chunk.js"
+  },
+  {
+    "revision": "e21f96c3572817207171",
+    "url": "./static/js/runtime-main.eb742682.js"
+  }
+]);
